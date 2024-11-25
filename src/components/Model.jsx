b02fb2c673adac557099b3e018f3aa56720993ec
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-11-25 23:58:06 +0530

    three js setup

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 // GSAP
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+// Component
+import ModelView from "./ModelView";
+// Asset
+import { yellowImg } from "../utils";
+// Three.js
+import * as THREE from "three";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "6F6C64"],
+    img: yellowImg,
+  });
+
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
+
   return (
     <section className="common-padding">
       <div className="screen-max-width">
@@ -15,7 +41,9 @@ const Model = () => {
         </h1>
 
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
         </div>
       </div>
     </section>

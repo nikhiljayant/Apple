0be560edd5f4687a42970fcd0439de3a63c69aab
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-11-27 23:58:52 +0530

    iphone model jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { Suspense } from "react";
+// Dependency
+import { PerspectiveCamera, View } from "@react-three/drei";
+// Component
+import Lights from "./Lights";
+import IPhone from "./IPhone";
 
-const ModelView = () => {
-  return <div>ModelView</div>;
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense fallback={<div>Loading</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
